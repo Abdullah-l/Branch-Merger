c790eee1bca84f10d11d66368032ddef7bc3c710
Abdullah Almanie <aalmanie@mrsool.co>
2022-12-18 10:52:37 +0300

    k

--- index.js
+++ index.js
@@ -1,45 +1,93 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@actions/core");
-const github_1 = require("@actions/github");
-const dedent_1 = require("dedent");
-const inputName = (0, core_1.getInput)("name");
-const ghToken = (0, core_1.getInput)("ghToken");
-greet(inputName, getRepoUrl(github_1.context));
-getDiff().then(files => {
-    console.log((0, dedent_1.default)(`
-    Your PR diff:
-    ${JSON.stringify(files, undefined, 2)}
-    `));
-});
-function greet(name, repoUrl) {
-    console.log(`'Hello ${name}! You are running a GH Action in ${repoUrl}'`);
-}
-function getRepoUrl({ repo, serverUrl }) {
-    return `${serverUrl}/${repo.owner}/${repo.repo}`;
-}
-function getDiff() {
-    return __awaiter(this, void 0, void 0, function* () {
-        if (ghToken && github_1.context.payload.pull_request) {
-            const octokit = (0, github_1.getOctokit)(ghToken);
-            const result = yield octokit.rest.repos.compareCommits({
-                repo: github_1.context.repo.repo,
-                owner: github_1.context.repo.owner,
-                head: github_1.context.payload.pull_request.head.sha,
-                base: github_1.context.payload.pull_request.base.sha,
-                per_page: 100
-            });
-            return result.data.files || [];
-        }
-        return [];
+const core = __importStar(require("@actions/core"));
+const github = __importStar(require("@actions/github"));
+const path_1 = __importDefault(require("path"));
+const simpleGit = require("simple-git");
+const baseDir = path_1.default.join(process.cwd(), '');
+console.log(baseDir);
+const git = simpleGit({ baseDir });
+const token = core.getInput('token');
+const labels = JSON.parse(core.getInput('labels'));
+const repoOwner = github.context.repo.owner;
+const repo = github.context.repo.repo;
+function pullRequests(repoOwner, repo) {
+    let client = github.getOctokit(core.getInput('token'));
+    let resp = client.rest.pulls.list({
+        owner: repoOwner,
+        repo: repo,
+    }).catch(e => {
+        core.setFailed(e.message);
     });
+    console.dir(resp, { depth: null });
+    core.info(`pullRequests: ${resp}`);
+    console.log(`pullRequests: ${resp}`);
+    return resp;
 }
+// function filterLabel(labels ,target: string[]):boolean{
+//     let labelname = labels.map((label) => {
+//         return label.name
+//     })
+//     let filterdLabels = labelname.filter(
+//         label => target.indexOf(label) != -1
+//     )
+//     if ( filterdLabels.length == target.length) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+async function setOutput(pull) {
+    let output = '';
+    for (const p of pull) {
+        console.log(p.head.ref);
+        console.log('\n');
+        output = output + p.title + "\\n" + p.html_url + "\\n---\\n";
+    }
+    try {
+        console.log("testdfjkgdrfjk");
+        await git.addConfig("user.name", "github-actions");
+        await git.listConfig();
+        // const branch = git.checkoutLocalBranch("test-branch");
+        console.log(await git.status());
+        // await git.push("origin", "test-branch");
+    }
+    catch (error) {
+        console.log(error);
+    }
+    console.log(output);
+    core.setOutput('pulls', output);
+    core.setOutput('daddy', output);
+}
+const now = Date.now();
+const prom = pullRequests(repoOwner, repo);
+prom.then((pulls) => {
+    let claim = pulls.data;
+    setOutput(claim);
+});
--- index.ts
+++ index.ts
@@ -1,43 +1,74 @@
-import { getInput } from "@actions/core";
-import { context, getOctokit } from "@actions/github";
-import dedent from 'dedent'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import path from 'path'
 
-type GithubContext = typeof context;
+const simpleGit = require("simple-git");
+const baseDir = path.join(process.cwd(),'')
+console.log(baseDir)
+const git = simpleGit({ baseDir })
 
-const inputName = getInput("name");
-const ghToken = getInput("ghToken");
 
-greet(inputName, getRepoUrl(context));
+const token: string = core.getInput('token')
+const labels: string[] = JSON.parse(core.getInput('labels'))
+const repoOwner: string = github.context.repo.owner
+const repo: string = github.context.repo.repo
 
-getDiff().then(files => {
-    console.log(dedent(`
-    Your PR diff:
-    ${JSON.stringify(files, undefined, 2)}
-    `))
+
+function pullRequests(repoOwner:string, repo:string ) {
+    let client = github.getOctokit(core.getInput('token'))
+    let resp = client.rest.pulls.list({
+        owner: repoOwner,
+        repo: repo,
+    }).catch(
+        e => {
+            core.setFailed(e.message)
+        }
+    )
+    console.dir(resp, { depth: null })
+    core.info(`pullRequests: ${resp}`)
+    console.log(`pullRequests: ${resp}`)
+    return resp
+}
+
+// function filterLabel(labels ,target: string[]):boolean{
+//     let labelname = labels.map((label) => {
+//         return label.name
+//     })
+//     let filterdLabels = labelname.filter(
+//         label => target.indexOf(label) != -1
+//     )
+//     if ( filterdLabels.length == target.length) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+async function setOutput(pull){
+    let output = ''
+    for (const p of pull) {
+        console.log(p.head.ref)
+        console.log('\n')
+        output = output + p.title + "\\n" + p.html_url + "\\n---\\n"
+    }
+    try {
+        console.log("testdfjkgdrfjk")
+        await git.addConfig("user.name", "github-actions");
+        await git.listConfig();
+        // const branch = git.checkoutLocalBranch("test-branch");
+        console.log(await git.status())
+        // await git.push("origin", "test-branch");
+    } catch (error) {
+        console.log(error);
+    }
+    console.log(output)
+    core.setOutput('pulls', output)
+    core.setOutput('daddy', output)
+}
+
+const now = Date.now()
+const prom = pullRequests(repoOwner,repo)
+prom.then((pulls: any) => {
+    let claim = pulls.data
+    setOutput(claim)
 })
-
-function greet(name: string, repoUrl: string) {
-  console.log(`'Hello ${name}! You are running a GH Action in ${repoUrl}'`);
-}
-
-function getRepoUrl({ repo, serverUrl }: GithubContext): string {
-  return `${serverUrl}/${repo.owner}/${repo.repo}`;
-}
-
-async function getDiff() {
-  if (ghToken && context.payload.pull_request) {
-      const octokit = getOctokit(ghToken)
-
-      const result = await octokit.rest.repos.compareCommits({
-          repo: context.repo.repo,
-          owner: context.repo.owner,
-          head: context.payload.pull_request.head.sha,
-          base: context.payload.pull_request.base.sha,
-          per_page: 100
-      })
-
-      return result.data.files || []
-  }
-
-  return []
-}
