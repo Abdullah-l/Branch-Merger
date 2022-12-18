76bc6a730995a24eb782ad5b7a811cbd8f342685
Abdullah Almanie <aalmanie@mrsool.co>
2022-12-18 13:24:51 +0300

    dsf

--- index.js
+++ index.js
@@ -76,10 +76,10 @@ async function setOutput(pull) {
         await git.addConfig("user.email", "gggg@gggg.com");
         await git.fetch();
         console.log(await git.status());
-        await git.checkout("origin/stag");
+        await git.checkout("stag");
         await git.reset("hard", ["origin/master"]);
-        // await git.mergeFromTo("origin/feat-no-conf", "origin/stag", ["--squash"]);
-        await git.push("origin");
+        await git.mergeFromTo("origin/feat-no-conf", "origin/stag", ["--squash"]);
+        await git.push("origin", "stag", ["--force"]);
     }
     catch (error) {
         console.log(error);
